ba9104b68485dddc5be8f9d6bd7979e213bba74d
Benjamin Wallberg <me@bwallberg.com>
2023-10-08 23:10:10 +0200

    docs: update

--- src/systems/MovementSystem.ts
+++ src/systems/MovementSystem.ts
@@ -15,7 +15,7 @@ export const MovementSystem = (ecs: ECS): System => ({
 			const physics = ecs?.get(entity, PhysicsComponent);
 
 			if (movement && physics) {
-				// Move the entity in the movement direction
+				// Move the entity in the desired direction by updating the velocity
 				const velocity = physics.velocity.clone();
 
 				switch (movement.direction) {
@@ -31,11 +31,17 @@ export const MovementSystem = (ecs: ECS): System => ({
 
 				physics.velocity = velocity;
 
+				// if jumping and y velocity is 0 ( not jumping or falling ) apply impulse to make the body "jump"
 				if (movement.state === MovementState.JUMPING && velocity.y === 0) {
 					physics.body.applyLinearImpulse(
 						new Vec2(0, -20),
 						physics.body.getWorldCenter(),
 					);
+				}
+
+				// check the velocity and determine the state of the movement
+				if (velocity.y < 0) {
+					movement.state = MovementState.JUMPING;
 				} else if (velocity.y > 0) {
 					movement.state = MovementState.FALLING;
 				} else if (velocity.y === 0) {
